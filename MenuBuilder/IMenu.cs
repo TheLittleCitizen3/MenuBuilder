2ad5b82c396b9f2a7cf2f941a20078f914170da7
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-18 16:40:29 +0300

    firs files

---
+++ MenuBuilder/IMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace MenuBuilder
+{
+    interface IMenu<T> : ITakeAction<T>
+    {
+        Dictionary<T,ITakeAction<T>> ActionItems { get; set; }
+    }
+}
---
+++ MenuBuilder/ITakeAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace MenuBuilder
+{
+    interface ITakeAction<T>
+    {
+        string ActionName { get; set; }
+        public int Action();
+
+    }
+}
