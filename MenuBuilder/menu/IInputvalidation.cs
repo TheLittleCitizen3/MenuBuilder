db7b3a962890e21df1ec234c41527c1af10e17a3
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-19 11:18:50 +0300

    added

---
+++ MenuBuilder/menu/IInputvalidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuBuilder.menu
+{
+    interface IInputvalidation
+    {
+        public bool Validate(string userInput);
+    }
+}
